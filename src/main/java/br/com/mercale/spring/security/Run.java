cb4fa2de37d743349295a8609635e24a2fae0989
heriveltonpaiva <heriveltonpaiva@gmail.com>
2018-03-19 00:05:32 -0300

    Implementação do Spring Security

---
+++ src/main/java/br/com/mercale/spring/security/Run.java
@@ -0,0 +1,11 @@
+package br.com.mercale.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/** https://memorynotfound.com/spring-boot-spring-security-thymeleaf-form-login-example/ **/
+@SpringBootApplication
+public class Run {
+    public static void main(String[] args) {
+        SpringApplication.run(Run.class, args);
+    }
+}
